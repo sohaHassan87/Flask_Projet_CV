f7fad7bd5a11a620b0974fa9ad7b6b6af4789731
sohaHassan87 <122526619+sohaHassan87@users.noreply.github.com>
2024-04-29 13:53:11 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__) #creating flask app name
 
-@app.route('/')
-def home():
-    return render_template("index.html")
+#@app.route('/')
+#def home():
+ #   return render_template("index.html")
 
-@app.route('/resume_1')
+@app.route('/')
 def resume_1():
     return render_template("resume_1.html")
 
